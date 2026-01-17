bfe2cc0c453c8db94e8e70b9c823e4b4d09ecdb5
elecTric <teeweipheng@gmail.com>
2026-01-17 15:36:47 +0800

    change main.py location and update API to use async/await

--- app/controller/scrape.py
+++ app/controller/scrape.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, HTTPException, Query
-from app.services.scraper import scrape_url
+from app.services.scraper import scrape_url_async
 from app.schemas.scrape import ScrapeResponse
 
 router = APIRouter()
 
 @router.get("/scrape", response_model=ScrapeResponse)
-def scrape_endpoint(
+async def scrape_endpoint(
     url: str = Query(..., description="Public URL to scrape")
 ):
     try:
-        return scrape_url(url)
+        return await scrape_url_async(url)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
--- app/services/scraper.py
+++ app/services/scraper.py
@@ -1,33 +1,30 @@
-import requests
+import httpx
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import socket
 import ipaddress
 
-def _is_valid_url(url: str) -> None:
+def _validate_url(url: str) -> None:
     parsed = urlparse(url)
     if parsed.scheme not in ("http", "https") or not parsed.netloc:
         raise ValueError("Invalid URL format")
 
-def _is_public_ip(url: str) -> None:
+def _block_private_ip(url: str) -> None:
     hostname = urlparse(url).hostname
     ip = socket.gethostbyname(hostname)
     if ipaddress.ip_address(ip).is_private:
         raise ValueError("Private or internal IP not allowed")
 
-def scrape_url(url: str) -> dict:
-    _is_valid_url(url)
-    _is_public_ip(url)
+async def scrape_url_async(url: str) -> dict:
+    _validate_url(url)
+    _block_private_ip(url)
 
-    #send request to the url
-    response = requests.get(
-        url,
+    async with httpx.AsyncClient(
         timeout=10,
-        headers={
-            "User-Agent": "FastAPI-Scraper/1.0"
-        }
-    )
-    response.raise_for_status()
+        headers={"User-Agent": "FastAPI-Scraper/1.0"}
+    ) as client:
+        response = await client.get(url)
+        response.raise_for_status()
 
     soup = BeautifulSoup(response.text, "lxml")
 
